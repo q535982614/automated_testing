e318bdadde12f02e33ab97393bbc95bb2131f2b6
LLLiPeng <Author Email>
2019-09-09 21:00:59 +0800

    login

--- apitest/views.py
+++ apitest/views.py
@@ -1,6 +1,26 @@
+from django.contrib import auth
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
+
+
+def login(request):
+    if request.POST:
+        username = password = ''
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None and user.is_active:
+            auth.login(request,user)
+            request.session['user'] = username
+            response = HttpResponseRedirect('/home/')
+            return response
+        else:
+            return render(request,'login.html',{'error':'username or password error'})
+
+    return render(request, 'login.html')
+
 
 def test(request):
     return HttpResponse('HELLO')
+
