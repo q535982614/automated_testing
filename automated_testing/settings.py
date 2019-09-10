3dd02baf965c7459708ea84c8e9307265ab85c5d
q535982614 <535982614@qq.com>
2019-09-10 20:40:12 +0800

    amdin 产品管理

--- automated_testing/settings.py
+++ automated_testing/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apitest.apps.ApitestConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from product.models import Product
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['productname', 'prodouctdesc', 'producter', 'create_time', 'id']
+
+
+admin.site.register(Product)  # 把产品模块注册到Django admin 后台并展示
